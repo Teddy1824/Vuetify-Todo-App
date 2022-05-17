16128c17c965d596c63425cc5fc3862eaa4d2d1c
Bongani Nomangola <bearnomangola7@gmail>
2022-05-17 09:37:21 +0200

    first vuetify project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about pa-6">
+    <h1>About page</h1>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et ea, nulla voluptate fuga rem provident magnam hic id quos esse libero modi aspernatur cupiditate harum totam? Ut earum ea quia autem dolore quibusdam cumque odio?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et ea, nulla voluptate fuga rem provident magnam hic id quos esse libero modi aspernatur cupiditate harum totam? Ut earum ea quia autem dolore quibusdam cumque odio?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et ea, nulla voluptate fuga rem provident magnam hic id quos esse libero modi aspernatur cupiditate harum totam? Ut earum ea quia autem dolore quibusdam cumque odio?</p>
+
+  </div>
+</template>
